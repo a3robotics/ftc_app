b98793fe6bd10f67cfe182f5e8204fcf871e7f4c
Benjamin <a3robotics@gmail.com>
2018-10-26 19:39:56 -0700

    Added Tank Drive skeleton class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+//imports
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="TankDrive",group="TankDrive")
+public class TankDrive {
+
+    // Runs when robot is turned on
+    public void init() {
+
+    }
+
+    // Runs when driver hits PLAY
+    public void start() {
+
+    }
+
+    // Executed continuously after driver hits PLAY
+    public void loop() {
+
+    }
+
+    // Runs when robot stops
+    public void stop() {
+
+    }
+}
