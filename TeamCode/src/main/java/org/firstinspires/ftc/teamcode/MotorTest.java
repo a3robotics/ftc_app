2b64754e3310f6b4c9b9308550864d159e969ee2
a3robotics <a3robotics@gmail.com>
2018-10-30 17:33:17 -0700

    Update for 2 motor Tank Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -16,6 +16,8 @@ public class MotorTest extends OpMode {
         // In the app, go to config and set the motor name to "motorTest"
         motorL = hardwareMap.dcMotor.get("motorLeft");
         motorR = hardwareMap.dcMotor.get("motorRight");
+        motorL.setDirection(DcMotor.Direction.REVERSE);
+
         motorL.setPower(0);
         motorR.setPower(0);
     }
@@ -28,6 +30,7 @@ public class MotorTest extends OpMode {
         double rightY = gamepad1.right_stick_y;
 
         // Set motor power to joystick Y value
+
         motorL.setPower(leftY);
         motorR.setPower(rightY);
     }
@@ -35,5 +38,12 @@ public class MotorTest extends OpMode {
         // Stop the motor
         motorL.setPower(0);
         motorR.setPower(0);
+    }
+
+    // todo function
+    void turn180() {
+        // set variables for encoder values
+        // while loop checking encoder value difference
+        // keep turning until they've gone a certain amount
     }
 }
