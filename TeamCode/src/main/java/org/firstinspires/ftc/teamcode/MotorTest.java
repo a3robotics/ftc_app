1d10dc4baf25d0367bf2530a142c219884afb5fa
a3robotics <a3robotics@gmail.com>
2018-11-02 12:53:49 -0700

    Add lift motor and telemetry data controlled by dpad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -11,16 +11,19 @@ public class MotorTest extends OpMode {
     //Variables
     private DcMotor motorL;
     private DcMotor motorR;
+    private DcMotor motorLift;
 
     public void init() {
-        // In the app, go to config and set the motor name to "motorTest"
+        // In the app, go to config and set the motor names
         motorL = hardwareMap.dcMotor.get("motorLeft");
         motorR = hardwareMap.dcMotor.get("motorRight");
+        motorLift = hardwareMap.dcMotor.get("motorLift");
         // left motor is backwards
         motorL.setDirection(DcMotor.Direction.REVERSE);
 
         motorL.setPower(0);
         motorR.setPower(0);
+        motorLift.setPower(0);
     }
     public void start() {
         // Nothing needs be done
@@ -30,14 +33,28 @@ public class MotorTest extends OpMode {
         double leftY = gamepad1.left_stick_y;
         double rightY = gamepad1.right_stick_y;
 
+        float driveSpeedFactor = 2/3;
         // Set motor power to joystick Y value
-        motorL.setPower(leftY);
-        motorR.setPower(rightY);
+        motorL.setPower(leftY*driveSpeedFactor);
+        motorR.setPower(rightY*driveSpeedFactor);
 
+        // Use right trigger to lift and lower lift
+        if(gamepad1.dpad_up){
+            motorLift.setPower(1);
+        }else if(gamepad1.dpad_down){
+            motorLift.setPower(-1);
+        }else{
+            motorLift.setPower(0);
+        }
+
+        // Send telemetry data - drive encoders
         int motorLpos = motorL.getCurrentPosition();
         telemetry.addData("Left Encoder Position:", motorLpos);
         int motorRpos = motorR.getCurrentPosition();
         telemetry.addData("Right Encoder Position:", motorRpos);
+        // Lift encoder
+        int motorLiftPos = motorLift.getCurrentPosition();
+        telemetry.addData("Lift Encoder:",motorLiftPos);
     }
     public void stop() {
         // Stop the motor
