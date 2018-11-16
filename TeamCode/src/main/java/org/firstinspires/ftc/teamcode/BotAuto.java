e73ed81b586102cdca3b7be691beab36409eaa1c
a3robotics <a3robotics@gmail.com>
2018-11-15 18:11:20 -0800

    Created the beginnings of the drive() function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotAuto.java
@@ -44,10 +44,10 @@ public class BotAuto extends LinearOpMode {
         }
         motorLift.setPower(0);
         sleep(1000);
-        // Then, back up robot
+        // Then, forward robot
         while(motorL.getCurrentPosition()>-1000&&motorL.getCurrentPosition()>-1000 && opModeIsActive()){ // 1000 picked as an arbitrary value. Optimize this.
-            motorL.setPower(-0.25);
-            motorR.setPower(-0.25);
+            motorL.setPower(0.25);
+            motorR.setPower(0.25);
             telemetry.addData("Left Encoder Position:", motorL.getCurrentPosition());
             telemetry.addData("Right Encoder Position:", motorR.getCurrentPosition());
         }
@@ -65,5 +65,34 @@ public class BotAuto extends LinearOpMode {
         motorL.setPower(0);
         motorR.setPower(0);
         motorLift.setPower(0);
+    }
+
+    // prototype functions
+
+    private void drive(int distance, int rotate) {
+        double r = 2.36; //radius of wheels, in inches
+        double encoderRadiansL = motorL.getCurrentPosition()*3.14159/180;
+        double encoderRadiansR = motorR.getCurrentPosition()*3.14159/180;
+        if (distance == 0) {
+            if(rotate == 0){
+                telemetry.addData("ERROR", "Both arguments of drive() are zero");
+            }else{
+                double rotateRad = rotate*3.14159/180;
+                while(encoderRadiansL < rotateRad && encoderRadiansR < rotateRad) {
+                    motorR.setPower(0.5);
+                    motorL.setPower(0.5);
+                }
+            }
+        }else if(rotate == 0){
+            double rotateRad = distance/r*180/3.14159; //radians
+            while(encoderRadiansL < rotateRad && encoderRadiansR < rotateRad) {
+                motorR.setPower(0.5);
+                motorL.setPower(0.5);
+            }
+
+        }else {
+            // error!
+        }
+        //FOR RESETTING MOTORS: motorL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 }
