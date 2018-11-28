45e0b9aef1042658c24624ada676ea6610b9acb1
a3robotics <a3robotics@gmail.com>
2018-11-27 17:25:07 -0800

    Update everything to do the thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareLiftBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareLiftBot.java
@@ -3,15 +3,39 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class HardwareLiftBot {
 
-    public DcMotor motorL;
-    public DcMotor motorR;
-    public DcMotor motorLift;
+    public DcMotor motorL = null;
+    public DcMotor motorR = null;
+    public DcMotor motorLift = null;
 
+    HardwareMap hwMap =  null;
+    private ElapsedTime period = new ElapsedTime();
+
+    public HardwareLiftBot(){
+
+    }
 
     public void init (HardwareMap ahwMap) {
+        hwMap = ahwMap;
 
+        motorL = hwMap.dcMotor.get("motorLeft");
+        motorR = hwMap.dcMotor.get("motorRight");
+        motorLift = hwMap.dcMotor.get("motorLift");
+        // left motor is backwards
+        motorL.setDirection(DcMotor.Direction.REVERSE);
+
+        // Start with all motors stopped
+        motorL.setPower(0);
+        motorR.setPower(0);
+        motorLift.setPower(0);
+    }
+
+    public void kill () {
+        motorL.setPower(0);
+        motorR.setPower(0);
+        motorLift.setPower(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveBot.java
@@ -4,65 +4,16 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import static java.lang.Math.abs;
 
 @TeleOp(name="TankDriveBot",group="TankDriveBot")
 public class TankDriveBot extends OpMode {
 
-    // used for helper classes
-    // private Robot robot;
-
-    //Variables
-    private DcMotor motorL;
-    private DcMotor motorR;
-    private DcMotor motorLift;
-    private DcMotor motorArmRotate;
-
-    private CRServo servoArmElbow;
-    private CRServo servoArmElbow2;
-    private Servo servoClaw;
-
-    // encoder vals
+    private HardwareLiftBot robot = new HardwareLiftBot();
     private int liftUpperLimit = 16000;
     private int liftLowerLimit = 0;
 
-    public void init() {
-        // In the app, go to config and set the motor names
-        motorL = hardwareMap.dcMotor.get("motorLeft");
-        motorR = hardwareMap.dcMotor.get("motorRight");
-        motorLift = hardwareMap.dcMotor.get("motorLift");
-        motorArmRotate = hardwareMap.dcMotor.get("motorArmRotate");
-        //servos
-        //servoArmBase = hardwareMap.servo.get("servoArmBase");
-        servoArmElbow = hardwareMap.crservo.get("servoElbow");
-        servoArmElbow2 = hardwareMap.crservo.get("servoElbow2");
-        servoClaw = hardwareMap.servo.get("servoClaw "); // NOT a CR servo
-
-        // left motor is backwards
-        motorL.setDirection(DcMotor.Direction.REVERSE);
-
-        // Start with all motors stopped
-        motorL.setPower(0);
-        motorR.setPower(0);
-        motorLift.setPower(0);
-        motorArmRotate.setPower(0);
-        servoArmElbow.setPower(0);
-        servoArmElbow2.setPower(0);
-        //servoArmBase.setPosition(0.5);
-
-    }
-
-    public void start() {
-        // Reset all encoders
-//        robot.resetEncoders(motorL);
-//        robot.resetEncoders(motorR);
-//        robot.resetEncoders(motorLift);
-//        robot.resetEncoders(motorArmRotate);
-    }
+    public void init() {robot.init(hardwareMap);}
+    public void start() {}
 
     public void loop() {
         // get joystick Y values
@@ -71,76 +22,38 @@ public class TankDriveBot extends OpMode {
 
         double driveSpeedFactor = 0.75;
         // Set motor power to joystick Y value
-        motorL.setPower(leftY * driveSpeedFactor);
-        motorR.setPower(rightY * driveSpeedFactor);
+        robot.motorL.setPower(leftY * driveSpeedFactor);
+        robot. motorR.setPower(rightY * driveSpeedFactor);
 
         // Use right trigger and bumper to lift and lower lift
-        if (gamepad1.right_bumper && (motorLift.getCurrentPosition() > liftUpperLimit || gamepad1.y)) {
-            motorLift.setPower(-0.5); // half speed per too much rpm
-        } else if (gamepad1.right_trigger > 0 && (motorLift.getCurrentPosition() < liftLowerLimit || gamepad1.y)) { // being at all pressed
-            motorLift.setPower(0.5);
+        if (gamepad1.right_bumper && (robot.motorLift.getCurrentPosition() > liftUpperLimit || gamepad1.y)) {
+            robot.motorLift.setPower(-0.5); // half speed per too much rpm
+        } else if (gamepad1.right_trigger > 0 && (robot.motorLift.getCurrentPosition() < liftLowerLimit || gamepad1.y)) { // being at all pressed
+            robot.motorLift.setPower(0.5);
         } else {
-            motorLift.setPower(0);
+            robot.motorLift.setPower(0);
         }
-        //-16000
-
-        // Use dpad to rotate arm on lift
-        if (gamepad1.dpad_left) {
-            motorArmRotate.setPower(-0.1); // low speed
-        } else if (gamepad1.dpad_right) {
-            motorArmRotate.setPower(0.1);
-        } else {
-            motorArmRotate.setPower(0);
-        }
-
-        // Run servos
-        //dpad up/down and left trigger/bumper
-        if (gamepad1.dpad_up) {
-            servoArmElbow.setPower(.5);
-        } else if (gamepad1.dpad_down) {
-            servoArmElbow.setPower(-.5);
-        } else {
-            servoArmElbow.setPower(0);
-        }
-
-        //second joint of the arm
-        if(gamepad1.left_bumper) servoArmElbow2.setPower(.5);
-        else if(gamepad1.left_trigger > 0) servoArmElbow2.setPower(-.5);
-        else servoArmElbow2.setPower(0);
 
         getTelemetryData();
-
         if(gamepad1.back){
-            killRobot();
+            robot.kill();
         }
     }
 
     public void stop() {
-        killRobot();
+        robot.kill();
     }
 
     private void getTelemetryData() {
         // Send telemetry data - drive encoders
-        int motorLpos = motorL.getCurrentPosition();
+        int motorLpos = robot.motorL.getCurrentPosition();
         telemetry.addData("Left Encoder Position:", motorLpos);
-        int motorRpos = motorR.getCurrentPosition();
+        int motorRpos = robot.motorR.getCurrentPosition();
         telemetry.addData("Right Encoder Position:", motorRpos);
         // Lift encoder
-        int motorLiftPos = abs(motorLift.getCurrentPosition());
+        int motorLiftPos = robot.motorLift.getCurrentPosition();
         telemetry.addData("Lift Encoder:",motorLiftPos);
-        //Arm rotation encoder
-        int motorArmPos = motorArmRotate.getCurrentPosition();
-        telemetry.addData("Arm Rotation Encoder:",motorArmPos);
-    }
 
-    private void killRobot() {
-        // Stop all motors
-        motorL.setPower(0);
-        motorR.setPower(0);
-        motorLift.setPower(0);
-        motorArmRotate.setPower(0);
-        servoArmElbow.setPower(0);
-        servoArmElbow2.setPower(0);
     }
 
 }
