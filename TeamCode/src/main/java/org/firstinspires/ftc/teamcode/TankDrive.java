c6fd4f77c3aaf9c77118a1dfbc6721c15a758ae9
Benjamin <a3robotics@gmail.com>
2018-12-04 17:56:20 -0800

    Added boilerplate for camera mineral detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="TankDrive",group="TankDrive")
-public class TankDrive {
+public class TankDrive extends OpMode {
 
     // Runs when robot is turned on
     public void init() {
