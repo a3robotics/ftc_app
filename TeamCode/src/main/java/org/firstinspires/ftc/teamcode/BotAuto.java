3bb7c4c4265e46f9a3a6bd1d4f5169f7dd2940e4
Bailey <a3robotics@gmail.com>
2018-12-13 16:44:31 -0800

    Made the encoders for drive motors reset when drive() is called

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotAuto.java
@@ -14,13 +14,12 @@ package org.firstinspires.ftc.teamcode;
  */
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import java.lang.Math;
 
 @Autonomous(name="BotAuto", group="BotAuto")
-@Disabled
 public class BotAuto extends LinearOpMode {
 
     private HardwareLiftBot robot = new HardwareLiftBot();
@@ -39,12 +38,12 @@ public class BotAuto extends LinearOpMode {
         if(baileyCode == false) lowerLiftAndDriveIntoCrater(); //That's a mighty big function. I think I want to shorten it.
 
         if(baileyCode == true) { //owo, what dis?
-            //If you make the code to work as thus, it is a lot less confusing, and it also allows for easier updating. Of course, the numbers here are only temporary, but it does a similar thing to lowerLiftAndDriveIntoCrater()
+            //If you make the code to work as thus, it is a lot less confusing, and it also allows for easier updating. Of course, the numbers here are only temporary,
+            // but it does a similar thing to lowerLiftAndDriveIntoCrater()
             lowerLift();
             drive(-12, .25);
             rotate(180);
             drive(12, 1);
-            robot.kill();
             killMotors();
         }
 
@@ -95,7 +94,7 @@ public class BotAuto extends LinearOpMode {
 
     private void turn180() {
         runtime.reset(); // redundancy for safety
-        while (runtime.time() < 1.8 && opModeIsActive()) { //This means that in a ratio of (1.8s:180deg), that degree would be travelled for every 0.01 secs. Who needs gyros!
+        while (runtime.time() < 1.8 && opModeIsActive()) { //This means that in a ratio of (1.8s:180deg), one degree would be travelled for every 0.01 secs. Who needs gyros!
             robot.motorL.setPower(0.6);
             robot.motorR.setPower(-0.6);
             telemetry.addData("Left Encoder Position:", robot.motorL.getCurrentPosition());
@@ -118,7 +117,11 @@ public class BotAuto extends LinearOpMode {
         long amtL, amtR;
         double rWheel = 2.36, cWheel = 14.83; //needed info: radius and circumference of wheel. No function really.
         double ticks = 24.03 * inches; /*24.03 is the amount of ticks that the encoder goes through for every inch the robot travels, and if you multiply this ratio by the amount of inches
-                                      you want to travel, then you get the amount of distance you want the bot to go.*/
+                                      you want to travel, throbot.motorL.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        robot.motorR.setMode(DcMotor.RunMode.RESET_ENCODERS);en you get the amount of distance you want the bot to go.*/
+
+        robot.motorL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.motorR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.motorL.setPower(speed);
         robot.motorR.setPower(speed);
         while(opModeIsActive()) { //This SHOULD work, but it may not. Needs testing.
@@ -150,21 +153,22 @@ public class BotAuto extends LinearOpMode {
         while (runtime.time() < time && opModeIsActive()) {
 
             if (degrees < 0) {
-                robot.motorL.setPower(-.6);
-                robot.motorR.setPower(.6);
+                robot.motorL.setPower(-speed);
+                robot.motorR.setPower(speed);
 
             } else if (degrees > 0) {
-                robot.motorL.setPower(.6);
-                robot.motorR.setPower(-.6);
+                robot.motorL.setPower(speed);
+                robot.motorR.setPower(-speed);
 
             } else {
-                telemetry.addData("ERROR: ", "Parameter degrees equals zero in rotate()");
+                telemetry.addData("ERROR: ", "Parameter \"degrees\" equals zero in rotate()");
             }
         }
         runtime.reset();
     }
     private void killMotors() {
         runtime.reset();
-        robot.kill();
+        robot.motorL.setPower(0);
+        robot.motorR.setPower(0);
     }
 }
