2edd59828c93f7bced3da01f2a112a24c753d2f5
Bailey <a3robotics@gmail.com>
2018-12-15 09:52:35 -0800

    hammered out some more bugs in drive()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotAuto.java
@@ -20,6 +20,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import java.lang.Math;
 
 import static java.lang.Math.abs;
+import static java.lang.Math.round;
 
 @Autonomous(name="BotAuto", group="BotAuto")
 public class BotAuto extends LinearOpMode {
@@ -118,9 +119,10 @@ public class BotAuto extends LinearOpMode {
     private void drive(double inches, double speed) { //updated forward for it to use the actual amount of ticks that our motors have per rotation (which is 356.3). Also, I added a speed parameter. --Bailey
         long amtL, amtR;
         double rWheel = 2.36, cWheel = 14.83; //needed info: radius and circumference of wheel. No function really.
-        double ticks = 24.03 * inches; /*24.03 is the amount of ticks that the encoder goes through for every inch the robot travels, and if you multiply this ratio by the amount of inches
-                                      you want to travel, throbot.motorL.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        robot.motorR.setMode(DcMotor.RunMode.RESET_ENCODERS);en you get the amount of distance you want the bot to go.*/
+        double ticks = 24.03 * inches; /*24.03 is the amount of ticks that the encoder goes through for every inch the robot travels, and if you multiply
+                                      this ratio by the amount of inches you want to travel, throbot.motorL.setMode(DcMotor.RunMode.RESET_ENCODERS);
+                                      robot.motorR.setMode(DcMotor.RunMode.RESET_ENCODERS);en you get the amount of distance you want the bot to go.*/
+        long iTicks = round(ticks);
 
         robot.motorL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.motorR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -133,21 +135,21 @@ public class BotAuto extends LinearOpMode {
         } else if (inches < 0) {
             robot.motorL.setPower(speed);
             robot.motorR.setPower(speed);
-            while (opModeIsActive()) { //This SHOULD work, but it may not. Needs testing.
-                amtL = robot.motorL.getCurrentPosition();
-                amtR = robot.motorR.getCurrentPosition();
-                telemetry.addData("Left Encoder Position:", robot.motorL.getCurrentPosition());
-                telemetry.addData("Right Encoder Position:", robot.motorR.getCurrentPosition());
-                if (inches > 0) {
-                    if (amtL < ticks && amtR < ticks)
-                        break;//Encoder Values are negative when the bot goes forward
-                } else if (inches < 0) {
-                    if (amtL > ticks && amtR > ticks) break;
-                } else break;
-            }
-            robot.motorL.setPower(0);
-            robot.motorR.setPower(0);
         }
+        while (opModeIsActive()) { //This SHOULD work, but it may not. Needs testing.
+            amtL = robot.motorL.getCurrentPosition();
+            amtR = robot.motorR.getCurrentPosition();
+            telemetry.addData("Left Encoder Position:", robot.motorL.getCurrentPosition());
+            telemetry.addData("Right Encoder Position:", robot.motorR.getCurrentPosition());
+            if (inches > 0) {
+                if (amtL < iTicks && amtR < iTicks)
+                    break;//Encoder Values are negative when the bot goes forward
+            } else if (inches < 0) {
+                if (amtL > iTicks && amtR > iTicks) break;
+            } else break;
+        }
+        robot.motorL.setPower(0);
+        robot.motorR.setPower(0);
     }
 
     private void rotate(int degrees) { //rotates the bot. This works based off of
