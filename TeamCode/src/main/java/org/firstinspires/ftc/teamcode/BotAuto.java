0db548657231a7dc7db697e5e4a29c0f5ad5f637
Bailey <a3robotics@gmail.com>
2018-12-15 16:06:32 -0800

    made code in botAuto that works for our broken mechanical

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotAuto.java
@@ -76,7 +76,6 @@ public class BotAuto extends LinearOpMode {
             telemetry.addData("Right Encoder Position:", robot.motorR.getCurrentPosition());
         }
 
-
         //rotate robot 180 degrees
         rotate(45);
         runtime.reset();
