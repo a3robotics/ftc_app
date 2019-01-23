68fb48a842c30805b1ab32b978eb849d55c3f9dc
Benjamin <a3robotics@gmail.com>
2019-01-22 17:15:42 -0800

    Added crater side auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Crater.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Crater auto", group="Crater auto")
+public class Auto_Crater extends LinearOpMode {
+
+    private HardwareLiftBot robot = new HardwareLiftBot(this);
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        waitForStart();
+
+        // Lower from Lift
+        robot.lowerLift();
+
+        // Turn towards depot
+        robot.rotateGyro(-90);
+
+        // Drive to crater
+        robot.driveByTime(72,1);
+
+        robot.kill();
+    }
+}
