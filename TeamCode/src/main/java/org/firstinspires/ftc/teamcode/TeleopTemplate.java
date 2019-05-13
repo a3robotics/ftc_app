65cff344bf68802d8a1015fd20a9c40da4a9cbb4
a3robotics <a3robotics@gmail.com>
2019-05-13 16:59:13 -0700

    skldjfhksjdhfkjsdhfkjsdhkfjhskdjfh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTemplate.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TeleopTemplate extends OpMode {
+
+    public void init() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void loop() {
+
+    }
+
+    public void stop() {
+
+    }
+}
